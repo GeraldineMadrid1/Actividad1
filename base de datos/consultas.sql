22e22a2b52124b0927adbe252c626c158bdecbc9
Geraldine Madrid Carvajal <36246565+GeraldineMadrid1@users.noreply.github.com>
2018-02-08 15:12:27 -0500

    Add files via upload

---
+++ base de datos/consultas.sql
@@ -0,0 +1,12 @@
+SELECT * FROM Alumnos WHERE Curso “motos”
+//seleccionar alumnos que estén en el curso de motos//
+
+SELECT *FROM Alumnos WHERE Intensidad “Alta”
+//seleccionar los alumnos que estén en los cursos con intensidad alta//
+
+SELECT *FROM Alumnos WHERE Intensidad “media”
+//seleccionar los alumnos que se encuentren en los cursos con una intensidad media//
+
+
+SELECT *FROM Alumnos WHERE Edad >18 
+//seleccionar los alumnos que son mayores de 18 años//
